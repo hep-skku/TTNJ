e0ebe99a31d7b01a56648581727dd45e2a5d12b1
Junghwan John Goh <jhgoh@cern.ch>
2015-07-20 16:52:29 +0200

    moving codes to CATTools

--- ProcessEvent/python/partonTop_cfi.py
+++ ProcessEvent/python/partonTop_cfi.py
@@ -2,4 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 partonTop = cms.EDProducer("PartonTopProducer",
     genParticles = cms.InputTag("prunedGenParticles"),
+    jetMinPt = cms.double(20),
+    jetMaxEta = cms.double(2.4),
+    jetConeSize = cms.double(0.4),
 )
--- ProcessEvent/src/Consumers.cc
+++ ProcessEvent/src/Consumers.cc
@@ -1,4 +1,3 @@
 #include "TTNJ/ProcessEvent/interface/LeptonConsumer.h"
 #include "TTNJ/ProcessEvent/interface/JetConsumer.h"
-#include "TTNJ/ProcessEvent/interface/GenericNtupleConsumer.h"
 
